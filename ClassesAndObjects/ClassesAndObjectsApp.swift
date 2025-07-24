06a6a7203b183055a2f1a1ba107361bd80dde1b8
Chaylen <chaylennguyentran@gmail.com>
2025-07-24 11:26:42 -0700

    Initial Commit

---
+++ ClassesAndObjects/ClassesAndObjectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClassesAndObjectsApp.swift
+//  ClassesAndObjects
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct ClassesAndObjectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
